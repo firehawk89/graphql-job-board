2158ad13a275d80cf25bbab21b19b661127fc76f
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-21 22:22:01 +0300

    adjust Job resolver

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,7 +1,13 @@
+import { getCompany } from "../db/companies.js";
 import { getJobs } from "../db/jobs.js";
+import { formatISODate } from "../utils/helpers.js";
 
 export const resolvers = {
   Query: {
     jobs: () => getJobs(),
+  },
+  Job: {
+    company: (job) => getCompany(job.companyId),
+    date: (job) => formatISODate(job.createdAt),
   },
 };
