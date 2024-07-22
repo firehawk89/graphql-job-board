d9e60ea084a0f854a940b46a423da0e9a4ef99c4
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-22 20:07:06 +0300

    add jon data fetch

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -4,7 +4,7 @@ const client = new GraphQLClient("http://localhost:9000/graphql");
 
 export const getJobs = async () => {
   const query = gql`
-    query {
+    query Jobs {
       jobs {
         id
         title
@@ -18,4 +18,24 @@ export const getJobs = async () => {
   `;
   const { jobs } = await client.request(query);
   return jobs;
+};
+
+export const getJob = async (jobId) => {
+  const query = gql`
+    query Job($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        description
+        date
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const variables = { id: jobId };
+  const { job } = await client.request(query, variables);
+  return job;
 };
