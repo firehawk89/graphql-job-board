7cfd983e663e74a2eb013e855830e272b3a59b68
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-22 22:50:59 +0300

    add not found error throw function

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,1 +1,9 @@
+import { GraphQLError } from "graphql";
+
 export const formatISODate = (isoDate) => isoDate.split("T")[0];
+
+export const throwNotFound = (message) => {
+  throw new GraphQLError(message, {
+    extensions: { code: "NOT_FOUND" },
+  });
+};
