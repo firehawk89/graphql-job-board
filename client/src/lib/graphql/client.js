daea5a6b8f8ff80e34e0f6bed206801a679fac85
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-25 22:58:31 +0300

    use api url

--- client/src/lib/graphql/client.js
+++ client/src/lib/graphql/client.js
@@ -1,7 +1,8 @@
 import { GraphQLClient } from "graphql-request";
 import { getAccessToken } from "../auth";
+import { API_URL } from "../../utils/constants";
 
-const gqlClient = new GraphQLClient("http://localhost:9000/graphql", {
+const gqlClient = new GraphQLClient(API_URL, {
   headers: () => {
     const accessToken = getAccessToken();
     if (!accessToken) {
