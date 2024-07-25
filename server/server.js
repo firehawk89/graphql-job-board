353bc85a2244a737ddb6250c4fd80dee7fcc38f5
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-25 20:22:17 +0300

    set up apollo context

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
 import { readFile } from "node:fs/promises";
 import { ApolloServer } from "@apollo/server";
 import { resolvers } from "./graphql/resolvers.js";
+import { getUser } from "./db/users.js";
 
 const PORT = 9000;
 
@@ -13,13 +14,20 @@ app.use(cors(), express.json(), authMiddleware);
 
 app.post("/login", handleLogin);
 
+const apolloContext = async ({ req }) => {
+  if (!req.auth) {
+    return {};
+  }
+  const user = await getUser(req.auth.sub);
+  return { user };
+};
 const typeDefs = await readFile("./graphql/schema.graphql", "utf-8");
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
 });
 await apolloServer.start();
-app.use("/graphql", apolloMiddleware(apolloServer));
+app.use("/graphql", apolloMiddleware(apolloServer, { context: apolloContext }));
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
