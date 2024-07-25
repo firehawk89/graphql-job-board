57ec4b227d3d8eadf1375e63e615c16671c76e6b
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-25 20:21:32 +0300

    set up auth token header

--- client/src/lib/graphql/client.js
+++ client/src/lib/graphql/client.js
@@ -1,5 +1,16 @@
 import { GraphQLClient } from "graphql-request";
+import { getAccessToken } from "../auth";
 
-const gqlClient = new GraphQLClient("http://localhost:9000/graphql");
+const gqlClient = new GraphQLClient("http://localhost:9000/graphql", {
+  headers: () => {
+    const accessToken = getAccessToken();
+    if (!accessToken) {
+      return {};
+    }
+    return {
+      Authorization: `Bearer ${accessToken}`,
+    };
+  },
+});
 
 export default gqlClient;
