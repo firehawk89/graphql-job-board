559f049c2555d233ca48b26b361dc200c9502494
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-26 21:44:35 +0300

    implement cache update when the job is created

--- client/src/lib/graphql/mutations.js
+++ client/src/lib/graphql/mutations.js
@@ -1,17 +1,32 @@
 import { gql } from "@apollo/client";
 import { apolloClient } from "../apollo/client";
+import { JobQuery } from "./queries";
 
 export const createJob = async ({ title, description }) => {
   const mutation = gql`
     mutation CreateJob($input: CreateJobInput!) {
       job: createJob(input: $input) {
         id
+        title
+        description
+        date
+        company {
+          id
+          name
+        }
       }
     }
   `;
   const { data } = await apolloClient.mutate({
     mutation,
     variables: { input: { title, description } },
+    update: (cache, { data }) => {
+      cache.writeQuery({
+        query: JobQuery,
+        variables: { id: data.job.id },
+        data,
+      });
+    },
   });
   return data.job;
 };
