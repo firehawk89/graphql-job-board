df6e40eeac7c0328f369ea87c4b698a8313d6f04
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-26 22:11:48 +0300

    add useJob hook

---
+++ client/src/hooks/useJob.js
@@ -0,0 +1,11 @@
+import { useQuery } from "@apollo/client";
+import { JobQuery } from "../lib/graphql/queries";
+
+const useJob = (jobId) => {
+  const { loading, error, data } = useQuery(JobQuery, {
+    variables: { id: jobId },
+  });
+  return { job: data?.job, loading, isError: !!error };
+};
+
+export default useJob;
