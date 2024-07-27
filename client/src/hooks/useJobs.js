2a90df3f5f1b8a4d36f52d3cc0585c958fee52bf
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-27 17:52:18 +0300

    provde limit and offset params for the useJobs hook, update return statement

--- client/src/hooks/useJobs.js
+++ client/src/hooks/useJobs.js
@@ -1,11 +1,18 @@
 import { useQuery } from "@apollo/client";
 import { JobsQuery } from "../lib/graphql/queries";
 
-const useJobs = () => {
+const useJobs = ({ limit, offset }) => {
   const { loading, error, data } = useQuery(JobsQuery, {
+    variables: { limit, offset },
     fetchPolicy: "network-only",
   });
-  return { jobs: data?.jobs, loading, isError: !!error, error };
+  return {
+    jobs: data?.jobs?.items,
+    totalCount: data?.jobs?.totalCount,
+    loading,
+    isError: !!error,
+    error,
+  };
 };
 
 export default useJobs;
