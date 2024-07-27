f074939679adc91878a86893d9afb32a43d7e9d4
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-27 11:42:57 +0300

    remove createJob function

--- client/src/lib/graphql/mutations.js
+++ client/src/lib/graphql/mutations.js
@@ -1,27 +1,11 @@
 import { gql } from "@apollo/client";
-import { apolloClient } from "../apollo/client";
-import { JobQuery } from "./queries";
 import { JobDetailFragment } from "./fragments";
 
-export const createJob = async ({ title, description }) => {
-  const mutation = gql`
-    mutation CreateJob($input: CreateJobInput!) {
-      job: createJob(input: $input) {
-        ...JobDetail
-      }
+export const CreateJobMutation = gql`
+  mutation CreateJob($input: CreateJobInput!) {
+    job: createJob(input: $input) {
+      ...JobDetail
     }
-    ${JobDetailFragment}
-  `;
-  const { data } = await apolloClient.mutate({
-    mutation,
-    variables: { input: { title, description } },
-    update: (cache, { data }) => {
-      cache.writeQuery({
-        query: JobQuery,
-        variables: { id: data.job.id },
-        data,
-      });
-    },
-  });
-  return data.job;
-};
+  }
+  ${JobDetailFragment}
+`;
