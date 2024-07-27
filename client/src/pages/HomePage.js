196a54e3e72f1f654423e0ade86b4fa98c36743a
Anton Bochkovskiy <unundertale1@ukr.net>
2024-07-27 17:52:49 +0300

    implement pagination logic

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import JobList from "../components/JobList";
 import useJobs from "../hooks/useJobs";
+import PaginationBar from "../components/PaginationBar";
+
+const JOBS_PER_PAGE = 10;
 
 function HomePage() {
-  const { jobs, loading, isError } = useJobs();
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * JOBS_PER_PAGE;
+  
+  const { jobs, totalCount, loading, isError } = useJobs({
+    limit: JOBS_PER_PAGE,
+    offset,
+  });
+
+  const totalPages = Math.ceil(totalCount / JOBS_PER_PAGE);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -16,6 +28,11 @@ function HomePage() {
     <div>
       <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
+      <PaginationBar
+        currentPage={page}
+        totalPages={totalPages}
+        onPageChange={setPage}
+      />
     </div>
   );
 }
